fe9053f64e275210d7425cd9f16bcbab6b2e21dd
Alex Alimov <aalimov@asu.edu>
2021-05-02 00:57:40 -0700

    Implement header component with styling

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../../assets/logo-letter.png';
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionLink,
+  OptionsContainer,
+} from './header.styles';
+
+const Header = () => (
+  <HeaderContainer>
+    <LogoContainer to="/">
+      <img src={logo} alt="Medicall Logo" />
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to="/list-practice">List your practice</OptionLink>
+      <OptionLink to="/help">Help</OptionLink>
+      <OptionLink to="/login">Log in</OptionLink>
+    </OptionsContainer>
+  </HeaderContainer>
+);
+
+export default Header;
