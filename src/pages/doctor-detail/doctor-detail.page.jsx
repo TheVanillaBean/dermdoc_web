73f1e656ca8febcbe6c84e1a804a7ac9a996e927
Alex Alimov <aalimov@asu.edu>
2021-05-06 15:28:08 -0700

    Connect doctor detail page to redux

--- src/pages/doctor-detail/doctor-detail.page.jsx
+++ src/pages/doctor-detail/doctor-detail.page.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectDoctor } from '../../redux/search/search.selectors';
 import { HomePageContainer } from './doctor-detail.styles';
-
 class DoctorDetail extends React.Component {
-  render() {
-    const { match } = this.props;
+  componentDidMount() {}
 
+  render() {
+    const {
+      routeName,
+      first_name,
+      last_name,
+      accepted_insurances,
+    } = this.props.doctor;
     return (
       <HomePageContainer>
-        <h1>Doctor-Detail {match.params.doctor_route}</h1>
+        <h1>
+          Doctor-Detail {routeName} {first_name} {last_name}{' '}
+          {accepted_insurances}
+        </h1>
       </HomePageContainer>
     );
   }
 }
 
-export default DoctorDetail;
+const mapStateToProps = createStructuredSelector({
+  doctor: selectDoctor,
+});
+
+export default connect(mapStateToProps)(DoctorDetail);
