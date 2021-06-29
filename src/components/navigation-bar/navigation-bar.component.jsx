f63db5737f3515ce271b27d84a54ed81c41a9533
Alex Alimov <aalimov@asu.edu>
2021-06-28 22:50:03 -0700

    Implement Nav Bar

---
+++ src/components/navigation-bar/navigation-bar.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+
+const NavigationBar = () => {
+  return (
+    <nav className="nav-bar">
+      <img src={logo} alt="Medicall logo" className="logo" />
+
+      <ul className="nav-list">
+        <li className="nav-list__item">
+          <Link className="nav-list__link" to="/list-practice">
+            List your practice
+          </Link>
+        </li>
+        <li className="nav-list__item">
+          <Link className="nav-list__link" to="/help">
+            Help
+          </Link>
+        </li>
+        <li className="nav-list__item">
+          <Link className="nav-list__link" to="/login">
+            Login
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavigationBar;
