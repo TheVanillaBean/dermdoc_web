c076cfb62a374c29d4bfadac88812edc18126447
Alex Alimov <aalimov@asu.edu>
2021-08-05 16:21:11 -0700

    Make additional styling changes

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -34,7 +34,7 @@ class SignIn extends Component {
   render() {
     return (
       <div className="sign-in">
-        <h2>I already have an account</h2>
+        <h2 className="title">I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
@@ -56,8 +56,7 @@ class SignIn extends Component {
           />
           <div className="buttons">
             <CustomButton className="custom-button" type="submit">
-              {' '}
-              Sign in{' '}
+              Sign in
             </CustomButton>
             <CustomButton
               className="custom-button"
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -51,20 +51,12 @@ class SignUp extends React.Component {
   };
 
   render() {
-    const { displayName, email, password, confirmPassword } = this.state;
+    const { email, password, confirmPassword } = this.state;
     return (
       <div className="sign-up">
-        <h2 className="title">I do not have a account</h2>
+        <h2 className="title">I do not have an account</h2>
         <span>Sign up with your email and password</span>
         <form className="sign-up-form" onSubmit={this.handleSubmit}>
-          <FormInput
-            type="text"
-            name="displayName"
-            value={displayName}
-            onChange={this.handleChange}
-            label="Display Name"
-            required
-          />
           <FormInput
             type="email"
             name="email"
