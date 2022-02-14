9619ef5307eb137ee5c1deedee809eacf195f506
Alex Alimov <aalimov@asu.edu>
2022-02-14 16:45:44 -0700

    Update faq

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -40,12 +40,24 @@ class HomePage extends React.Component {
     title: 'Common Questions',
     rows: [
       {
-        title: 'How is Medicall different?',
-        content: `Short version: Same price as other custom skincare companies, but better product. <br /> <br /> When you get a custom topical cream from the most popular custom skincare companies (e.g. curology, apostrophe, hims/hers), your cream will have a lower concentration of active ingredients than what you would get by going to a dermatologist in-person. With Medicall, you get the same convenience of the big companies, but with the quality of an in-person dermatology visit.`,
+        title: '1. What is the custom formula (i.e. cream)?',
+        content: `Your custom formula is a custom cream made for your unique skin by your dermatologist. It will be designed for your needs and goals to target skin concerns: type of acne, redness, discoloration, texture, and more. Your dermatologist will review your answers and photos, and prescribe custom ingredients &#8212; at a custom strength &#8212; for your skin's unique needs. Before each new shipment, we'll check in to see how you're doing and if needed, we'll adjust your prescription to ensure it is always the right fit for your skin.`,
       },
       {
-        title: 'Is there a refund policy?',
-        content: `If your skin does not improve after using your custom formula, we'll issue a full-refund for every month used.`,
+        title: '2. What are the key ingredients?',
+        content: `Your active ingredients and their strength level will be picked for your skin by your dermatologist. They may include: tretinoin, azelaic acid, tranexamic acid, niacinamide, and more. In addition to active ingredients, your formula will also include inactive, or base ingredients that help the medications blend into a silky smooth cream. All ingredients are vegan, cruelty-free, unscented, and paraben-free.`,
+      },
+      {
+        title: '3. Who are the dermatologists?',
+        content: `Our team of dermatologists are medical doctors who have completed residency training in dermatology (i.e. they've gone to school for a really long time). They have all been board certified by the American Board of Dermatology. We'll match you with one who is licensed in your state and he/she will become an expert on your unique skin. They'll design your custom formula, answer questions, and make adjustments as needed. So don't be a stranger. You can always message your dermatologist through your DermDoc account if you have questions. We're here for you.`,
+      },
+      {
+        title: '4. How much does an DermDoc Subscription cost?',
+        content: `We send prescriptions as 3-month supplies. Each 3-month supply costs $45 (shipping included). We'll send you a notice for a refill every 3 months. That breaks down to just $15 per month. Cancel anytime.`,
+      },
+      {
+        title: '5. How does the 90-day guarantee work?',
+        content: `Love your DermDoc products in 90 days, or get your money back. You must contact support@dermdoc.com to cancel your subscription and request a refund at any time on or before the 90th day after you received your treatment plan.`,
       },
     ],
   };
