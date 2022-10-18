ce013e78184902176df8e3a1e0b9ba08fc199641
Alex Alimov <aalimov@asu.edu>
2022-10-18 00:47:00 -0700

    Create Suggestion component

---
+++ src/components/autocomplete/suggestion.component.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Suggestion extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isHovered: false,
+    };
+  }
+
+  formatAutocompleteSuggestion = (suggestion) => {
+    const street = suggestion.streetLine ? `${suggestion.streetLine} ` : '';
+    const secondary = suggestion?.secondary ? `${suggestion.secondary} ` : '';
+    const entries = suggestion?.entries > 1 ? `(${suggestion.entries} more entries) ` : '';
+    const city = suggestion?.city ? `${suggestion.city} ` : '';
+    const state = suggestion?.state ? `${suggestion.state}, ` : '';
+    const zip = suggestion?.zipcode ? `${suggestion.zipcode}` : '';
+
+    return street + secondary + entries + city + state + zip;
+  };
+
+  setIsHovered = (isHovered) => {
+    this.setState({ isHovered });
+  };
+
+  buildResultHoverClass = () => {
+    const className = 'autocomplete--suggestion';
+    return this.state.isHovered ? className + ' autocomplete--suggestion-hover' : className;
+  };
+
+  render() {
+    return (
+      <div
+        className={this.buildResultHoverClass()}
+        onClick={this.props.selectSuggestion}
+        onMouseEnter={() => this.setIsHovered(true)}
+        onMouseLeave={() => this.setIsHovered(false)}>
+        {this.formatAutocompleteSuggestion(this.props.suggestion)}
+      </div>
+    );
+  }
+}
+
+export default Suggestion;
