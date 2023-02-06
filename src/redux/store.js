ac96ee5a78e8e8f2ea5b330f7769abf7c7095544
Alex Alimov <aalimov@asu.edu>
2023-02-06 15:06:53 -0700

    Remove old tracking code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,15 @@
-import { lazy } from 'react';
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
 import rootReducer from './root-reducer';
-const logger = lazy(() => import('redux-logger'));
+// const logger = lazy(() => import('redux-logger'));
 
 const middlewares = [thunk];
 
-if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger);
-}
+// if (process.env.NODE_ENV === 'development') {
+//   middlewares.push(logger);
+// }
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 
